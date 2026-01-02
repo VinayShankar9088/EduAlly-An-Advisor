6d05b0d90286330187aef5777fac6b61c6d71915
VinayShankar9088 <panditji9088xyz@gmail.com>
2026-01-02 12:03:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -101,7 +101,6 @@ def save_student():
 
 
 if __name__ == "__main__":
-	init_db()
-	print("Starting EduAlly app on http://127.0.0.1:5000")
-	app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
 
